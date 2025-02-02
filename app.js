26fa5895fbc9d8bb960ae81b8580733730da90b6
BananILN <daniil.klovans84@mail.ru>
2025-02-02 18:37:48 +0400

    get console Types

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
 "use strict";
+// Шаг 1-2: Создание переменных и вывод в консоль
 console.log("Hello TypeScript");
+// Примитивные типы
+let str = "Hello World";
+let num = 42;
+let bool = true;
+let nullValue = null;
+let undefinedValue = undefined;
+let arr = [1, 2, 3];
+console.log("Исходные значения:");
+console.log({ str, num, bool, nullValue, undefinedValue, arr });
+// Шаг 3: Изменение значений и повторный вывод
+str = "Updated string";
+num = 100;
+bool = false;
+arr.push(4);
+console.log("\nИзмененные значения:");
+console.log({ str, num, bool, nullValue, undefinedValue, arr });
+let numbers = [5, 2, 8, 1];
+console.log("\nРабота с массивом:");
+console.log("Исходный массив:", numbers);
+// Добавление элемента
+numbers.push(4);
+console.log("После push(4):", numbers);
+// Удаление последнего элемента
+numbers.pop();
+console.log("После pop():", numbers);
+// Получение длины
+console.log("Длина массива:", numbers.length);
+// Сортировка
+numbers.sort((a, b) => a - b);
+console.log("После сортировки:", numbers);
+// Дополнительные типы из примера пользователя (для демонстрации)
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Red;
+let tuple = ["Alex", 33, true];
+console.log("\nДополнительные элементы:");
+console.log({ c, tuple });
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,46 @@
 console.log("Hello TypeScript");
+
+let str: string = "Hello World";
+let num: number = 42;
+let bool: boolean = true;
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+let arr: number[] = [1, 2, 3];
+
+console.log("Исходные значения:");
+console.log({ str, num, bool, nullValue, undefinedValue, arr });
+
+str = "Updated string";
+num = 100;
+bool = false;
+arr.push(4);
+
+console.log("\nИзмененные значения:");
+console.log({ str, num, bool, nullValue, undefinedValue, arr });
+
+let numbers: number[] = [5, 2, 8, 1];
+
+console.log("\nРабота с массивом:");
+console.log("Исходный массив:", numbers);
+
+numbers.push(4);
+console.log("После push(4):", numbers);
+
+
+numbers.pop();
+console.log("После pop():", numbers);
+
+
+console.log("Длина массива:", numbers.length);
+
+
+numbers.sort((a, b) => a - b);
+console.log("После сортировки:", numbers);
+
+
+enum Color { Red, Green, Blue }
+let c: Color = Color.Red;
+
+let tuple: [string, number, boolean] = ["Alex", 33, true];
+console.log("\nДополнительные элементы:");
+console.log({ c, tuple });
